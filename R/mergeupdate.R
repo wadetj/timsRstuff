4ff031a798fd9b7a68bc9233235d19d589e0d9ca
wadetj <wadetj@gmail.com>
2022-07-07 15:59:13 -0400

    working draft of mergeupdate function

--- R/mergeupdate.R
+++ R/mergeupdate.R
@@ -4,32 +4,28 @@
 #variables not in dataset1 but in dataset2 should be added to dataset1
 #seems to work- does not handle many to 1 merge
 #need to add checks if ids are unique
-#add merge type for 1:1 and 1:m
-
-
-
+#add merge type for 1:1 and 1:m (df1- single observation, df2, multiple obs)
+#1 to many merges expands original dataset(df1), creates new id and duplicates
+#values for which there are no values in second dataset
+#also 1:m resorts dataset based on id
 
 mergeupdate<-function(df1, df2, id, id1, id2, mergetype="1:1"){
  newdf<-df1
-  
+ 
   if(!is.na(id)){
     id1=id
     id2=id
   }
 
- #does not seem to work
-  #if(mergetype=="1:1") {
-   if(anyDuplicated(id1)>0 | anyDuplicated(id2)>0) {
-     stop("ids do not uniquely identify an observation")
-   }
-# }
+  if(mergetype=="1:1") {
+   if(anyDuplicated(df1[, id1])>0 | anyDuplicated(df2[, id2])>0) stop("ids do not uniquely identify an observation. Use mergetype='1:m' for many to one merge")
+  }
  
  id1x<-paste0("^", id1, "$")
  id2x<-paste0("^", id1, "$")
  
   df1.vars<-names(df1)
   df2.vars<-names(df2)
-  
   
   xx<-grep(id1x, df1.vars)
   df1.vars<-df1.vars[-xx]
@@ -39,26 +35,31 @@ mergeupdate<-function(df1, df2, id, id1, id2, mergetype="1:1"){
   df1only<-df1.vars[!df1.vars %in% df2.vars]
   df2only<-df2.vars[!df2.vars %in% df1.vars]
  
+  #creates new id for multiple observations
+  #expands original dataset 
+  
+  if(mergetype=="1:m") {
+    ord2<-order(df2[,id2])
+    df2<-df2[ord2,]
+    ord1<-order(newdf[,id1])
+    newdf<-newdf[ord1, ]
+    xtemp<-rle(df2[,id2])
+    times<-xtemp$lengths
+    newid<-sequence(xtemp$lengths)
+    df2newid<-paste0(df2[,id2], ".",  newid)
+    newdf<-as.data.frame(lapply(newdf, rep, times))
+    df2[,id2]<-df2newid
+    newdf[,id1]<-df2newid
+  }
+  
 if(length(update.vars)>0 ) {   
   for(i in 1:length(update.vars)) {
-    #varname<-paste(update.vars[i])
-    #newdf[,varname][match(newdf[,id1], df2[,id2])]<-df2[,varname]
     newdf[,update.vars[i]][match(newdf[,id1], df2[,id2])]<-df2[,update.vars[i]]
     newdf[, update.vars[i]][match(newdf[,id1], df2[,id2]) & is.na(df2[, update.vars[i]])]<-df1[, update.vars[i]][match(newdf[,id1], df2[,id2]) & is.na(df2[, update.vars[i]])]
     
   }
 }
-  
-  
-  
- 
-  #  for(i in 1:length(df1only)) {
-  #   varname<-paste(df1only[i])
-  #   newdf[,varname]<-df1[,varname]
-  #   
-  # }
 
-  
 if(length(df2only)>0){    
   for(i in 1:length(df2only)) {
     varname<-paste(df2only[i])
@@ -71,21 +72,19 @@ if(length(df2only)>0){
   
 }
 
-
-data(iris)
-iris2<-iris
-iris$id<-1:dim(iris)[1]
-iris2<-iris
-iris2$Sepal.Length<-rnorm(150)
-iris2$Petal.Width[25:75]<-rnorm(51)
-iris2$color="red"
-iris$height="tall"
-xx<-mergeupdate(iris, iris2, id="id")
-
 # 
-# dumb<-function(a, b, c) {
-#   if(a!=10) stop("a must equal 10")
-#   print("yes a=10")               
-# }
+# data(iris)
+# iris2<-iris
+# iris$id<-1:dim(iris)[1]
+# iris2<-iris
+# iris2$Sepal.Length<-rnorm(150)
+# iris2$Petal.Width[25:75]<-rnorm(51)
+# iris2$color="red"
+# iris$height="tall"
+# xx<-mergeupdate(iris, iris2, id="id")
 # 
-#   
+# data1<-data.frame(id=rep(1:10), x=1, y=10*runif(10), z=10)
+# data3<-data.frame(id=rep(1:10, 3), z=rnorm(30), y=100*runif(30))
+# mergeupdate(data1, data3, id="id", mergetype="1:m")
+# 
+
